bb3eac2f1f9f6766c7f99f730d7a0a6b179db193
João Paulo Costa <joopaulocostaleitepaulocosta7@gmail.com>
2025-10-17 11:55:41 -0300

    Commit 3

--- first_app_md15.py
+++ first_app_md15.py
@@ -60,84 +60,81 @@ from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 
 
+# st.write("## Testando a aplicação de um quadro desenhavel")
+
+# import os
+
+# import numpy as np
+# import streamlit as st
+# from os.path import dirname
+# from os.path import join
+# import setuptools
 
 
+# def readme() -> str:
+#     """Utility function to read the README file.
+#     Used for the long_description.  It's nice, because now 1) we have a top
+#     level README file and 2) it's easier to type in the README file than to put
+#     a raw string in below.
+#     :return: content of README.md
+#     """
+#     return open(join(dirname(__file__), "README.md")).read()
 
 
-st.write("## Testando a aplicação de um quadro desenhavel")
+# setuptools.setup(
+#     name="streamlit-drawable-canvas",
+#     version="0.0.1",
+#     author="Fanilo ANDRIANASOLO",
+#     author_email="andfanilo@gmail.com",
+#     description="A Streamlit custom component for a free drawing canvas using Fabric.js.",
+#     long_description=readme(),
+#     long_description_content_type="text/plain",
+#     url="https://github.com/andfanilo/streamlit-drawable-canvas",
+#     packages=setuptools.find_packages(),
+#     include_package_data=True,
+#     classifiers=[],
+#     python_requires=">=3.6",
+# )
+# _RELEASE = False  # on packaging, pass this to True
 
-import os
-
-import numpy as np
-import streamlit as st
-from os.path import dirname
-from os.path import join
-import setuptools
-
-
-def readme() -> str:
-    """Utility function to read the README file.
-    Used for the long_description.  It's nice, because now 1) we have a top
-    level README file and 2) it's easier to type in the README file than to put
-    a raw string in below.
-    :return: content of README.md
-    """
-    return open(join(dirname(__file__), "README.md")).read()
-
-
-setuptools.setup(
-    name="streamlit-drawable-canvas",
-    version="0.0.1",
-    author="Fanilo ANDRIANASOLO",
-    author_email="andfanilo@gmail.com",
-    description="A Streamlit custom component for a free drawing canvas using Fabric.js.",
-    long_description=readme(),
-    long_description_content_type="text/plain",
-    url="https://github.com/andfanilo/streamlit-drawable-canvas",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    classifiers=[],
-    python_requires=">=3.6",
-)
-_RELEASE = False  # on packaging, pass this to True
-
-if not _RELEASE:
-    _component_func = st.declare_component("st_canvas", url="http://localhost:3001",)
-else:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = st.declare_component("st_canvas", path=build_dir)
+# if not _RELEASE:
+#     _component_func = st.declare_component("st_canvas", url="http://localhost:3001",)
+# else:
+#     parent_dir = os.path.dirname(os.path.abspath(__file__))
+#     build_dir = os.path.join(parent_dir, "frontend/build")
+#     _component_func = st.declare_component("st_canvas", path=build_dir)
 
 
-def st_canvas(
-    brush_width=20,
-    brush_color="black",
-    background_color="#eee",
-    height=400,
-    width=600,
-    key="canvas",
-):
-    """ Validate inputs + Post-process image from canvas
-        :param brush_width:
-        :param brush_color:
-        :param background_color:
-        :param height:
-        :param width:
-        :param key:
-        :return: Reshaped image
-        """
-    component_value = _component_func(
-        brush_width=brush_width,
-        brush_color=brush_color,
-        background_color=background_color,
-        height=height,
-        width=width,
-        key=key,
-        default=None,
-    )
-    if component_value is None:
-        return None
+# def st_canvas(
+#     brush_width=20,
+#     brush_color="black",
+#     background_color="#eee",
+#     height=400,
+#     width=600,
+#     key="canvas",
+# ):
+#     """ Validate inputs + Post-process image from canvas
+#         :param brush_width:
+#         :param brush_color:
+#         :param background_color:
+#         :param height:
+#         :param width:
+#         :param key:
+#         :return: Reshaped image
+#         """
+#     component_value = _component_func(
+#         brush_width=brush_width,
+#         brush_color=brush_color,
+#         background_color=background_color,
+#         height=height,
+#         width=width,
+#         key=key,
+#         default=None,
+#     )
+#     if component_value is None:
+#         return None
 
-    w = component_value["width"]
-    h = component_value["height"]
-    return np.reshape(component_value["data"], (h, w, 4))
+#     w = component_value["width"]
+#     h = component_value["height"]
+#     return np.reshape(component_value["data"], (h, w, 4))
+
